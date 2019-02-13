f6b9e755884593f3679999d60ed9b83b495c7df4
mhussainshah1 <muhammadshah.14@sunymaritime.edu>
2019-02-13 14:00:29 -0500

    Updated Driver.java

--- src/com/company/Driver.java
+++ src/com/company/Driver.java
@@ -1,13 +1,13 @@
 package com.company;
 
 public class Driver extends Person {
-//    boolean organDonor;
-//
-//    public boolean isOrganDonor() {
-//        return organDonor;
-//    }
-//
-//    public void setOrganDonor(boolean organDonor) {
-//        this.organDonor = organDonor;
-//    }
+    boolean organDonor;
+
+    public boolean isOrganDonor() {
+        return organDonor;
+    }
+
+    public void setOrganDonor(boolean organDonor) {
+        this.organDonor = organDonor;
+    }
 }
